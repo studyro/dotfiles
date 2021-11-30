12e8902f5b4521660124a0c22b1a212e5a5b7277
studyro <studyro@gmail.com>
2021-11-30 23:41:20 +0800

    Recover fzf.zsh settings.

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -1,13 +1,16 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/peterzhang/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/peterzhang/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/peterzhang/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/peterzhang/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
+
+export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude .git"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
